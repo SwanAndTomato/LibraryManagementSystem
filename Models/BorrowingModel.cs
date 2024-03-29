96648e6bc7e832a0e8e2f2fbad2533d4335eecb2
SwanAndTomato <alex.duncan1993@gmail.com>
2024-03-29 14:50:03 +0000

    Updates to Views, Models and Repos

--- Models/BorrowingModel.cs
+++ Models/BorrowingModel.cs
@@ -1,27 +1,27 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-using System.Reflection.PortableExecutable;
 
-namespace LibraryManagementSystem.Models
+public class BorrowingModel
 {
-    public class BorrowingModel
-    {
-        public int Id { get; set; }
+    public int Id { get; set; }
 
-      
-        [Required(ErrorMessage = "Book ID is required")]
-        public int BookId { get; set; }
+    [Display(Name = "Book")]
+    public int BookId { get; set; }
 
-        [Required(ErrorMessage = "Reader ID is required")]
-        public int ReaderId { get; set; }
+    [Display(Name = "Book Title")]
+    public string BookTitle { get; set; }
 
-        [Display(Name = "Borrow Date")]
-        [DataType(DataType.Date)]
-        [Required(ErrorMessage = "Borrow date is required")]
-        public DateTime BorrowDate { get; set; }
+    [Display(Name = "Reader")]
+    public int ReaderId { get; set; }
 
-        [Display(Name = "Return Date")]
-        [DataType(DataType.Date)]
-        public DateTime? ReturnDate { get; set; }
-    }
+    [Display(Name = "Reader Name")]
+    public string ReaderName { get; set; }
+
+    [Display(Name = "Borrow Date")]
+    [DataType(DataType.Date)]
+    public DateTime BorrowDate { get; set; }
+
+    [Display(Name = "Return Date")]
+    [DataType(DataType.Date)]
+    public DateTime? ReturnDate { get; set; }
 }
